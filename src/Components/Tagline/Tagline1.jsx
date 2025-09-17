7be5a7d6fec6b96db236adcfe0f5795caba43d5b
samstringzz <freetlessguitarist@gmail.com>
2025-09-17 19:47:54 +0100

    update on texten temp

--- src/Components/Tagline/Tagline1.jsx
+++ src/Components/Tagline/Tagline1.jsx
@@ -1,16 +1,20 @@
-import SectionTitle from "../Common/SectionTitle";
-
 const Tagline1 = () => {
     return (
         <div className="tagline sp">
           <div className="container">
             <div className="row">
               <div className="col-lg-8 m-auto text-center">
-                <div className="heading1">
-                    <SectionTitle
-                        SubTitle="Tagline"
-                        Title="Empowering entrepreneurs to build brands they love"
-                    ></SectionTitle>
+                <div className="tagline-content">
+                  <h3 className="tagline-subtitle">Empowering entrepreneurs to build brands they love</h3>
+                  <h4 className="content-hub">Content Hub</h4>
+                  <div className="social-icons">
+                    <div className="social-icon">
+                      <i className="bi bi-instagram"></i>
+                    </div>
+                    <div className="social-icon">
+                      <i className="bi bi-youtube"></i>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
